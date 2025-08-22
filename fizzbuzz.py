acb413e5762c3813f4be50481be4659ba5db2cde
Raprok612 <raprok612@gmail.com>
2025-08-22 12:21:14 +0900

    feat: modified fizzbuzz.py

--- fizzbuzz.py
+++ fizzbuzz.py
@@ -1,8 +1,10 @@
-for i in range(1,16+1):
+for i in range(1,90+1):
 
     if i % 3 == 0:
         print('fizz')
     elif i % 5 == 0:
         print('buzz')
+    elif i % 15 == 0:
+        print('fizzbuzz')
     else:
         print(i)
